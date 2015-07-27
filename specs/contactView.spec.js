c08affecf7bce309d94b1e8e8403428e4b5fbfa2
Nikitas <nikitas.frantzeskakis@gmail.com>
2015-07-27 14:04:53 +0000

    Edited package.json main field

---
+++ specs/contactView.spec.js
@@ -0,0 +1,22 @@
+describe('contactView', function() {
+    var contactView;
+    
+    before(function(){
+        contactView = require('../js/views/contact')();
+    })
+
+    describe('#onSync', function(){
+        it('should trigger contact:change and call render', function(){
+            /*var triggerspy = sinon.spy(contactView, 'trigger');
+            var renderspy = sinon.spy(contactView,'render');
+
+            contactView.onSync();
+
+            assertTrue(triggerspy.calledWith('contact:change'));
+            assertTrue(renderspy.called)
+
+            contactView.trigger.restore();*/
+        })
+    })
+
+})
